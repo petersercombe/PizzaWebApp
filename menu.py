19e28e40bed51172f043f1c7439bb20a643de562
Learner.Link Admin <help@learner.link>
2022-04-29 13:17:13 +1000

    Friday 29th April

--- menu.py
+++ menu.py
@@ -8,6 +8,18 @@ menu = {"meatlovers":{"title":"BBQ Meatlovers",
                       "description":"Crispy rasher bacon, pepperoni & Italian sausage, topped with fresh mushrooms, capsicum, crumbled beef & juicy pineapple, finished with vibrant spring onions & oregano",
                       "price":10.95,
                       "kj":4936,
-                      "image":"supreme.webp"}
+                      "image":"supreme.webp"},
+        "vegorama":{"title":"Vegorama",
+                      "description":"Mushrooms, diced tomato, capsicum, baby spinach & slices of red onion, topped with crumbled feta cheese, kalamata olives & oregano",
+                      "price":10.95,
+                      "kj":4120,
+                      "image":"vegorama.webp"},
+        "pepporoni":{"title":"Pepporoni",
+                      "description":"A Domino's classic. Slices of crispy pepperoni & creamy mozzarella",
+                      "price":5.95,
+                      "kj":4456,
+                      "image":"pepporoni.webp"}
+
+
 
 }
