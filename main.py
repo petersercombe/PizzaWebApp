133146353446538a64a970c862b808cfe97326ef
Learner.Link Admin <help@learner.link>
2022-06-01 09:03:13 +1000

    Tuesday 31st May

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from menu import *
 
 # Initialise the web app instance
 app = Flask(__name__)
-
-orderData = {}
+app.secret_key = "AnythingYouWant"
+# orderData = {}
 
 @app.route("/")
 @app.route("/home")
@@ -13,34 +13,36 @@ def home():
 
 @app.route("/order")
 def order():
-    collection = request.args["collection"]
-    return render_template("order.html", collection=collection, menu=menu)
+    session["orderData"]["collection"] = request.args["collection"]
+    session.modified = True
+    return render_template("order.html", collection=session["orderData"]["collection"], menu=menu)
 
 @app.route("/customise")
 def customise():
-    collection = request.args["collection"]
-    selection = request.args["selection"]
+    session["orderData"]["selection"] = request.args["selection"]
     return render_template("customise.html",
-                           collection=collection,
+                           collection=session["orderData"]["collection"],
                            menu=menu,
                            customisations=customisations,
-                           selection=selection)
+                           selection=session["orderData"]["selection"])
 
 
 @app.route("/cart")
 def cartView():
-    return render_template("cart.html", orderData=orderData, menu=menu) # lots to do here
+    return render_template("cart.html", orderData=session["orderData"], menu=menu) # lots to do here
 
 
 @app.route("/cart", methods = ["post"])
 def cartPost():
-    orderData[str(len(orderData))] = request.form.to_dict()
-    print(orderData)
+    session["orderData"][str(len(session["orderData"]))] = request.form.to_dict()
+    session.modified = True
+    print(session["orderData"])
     return redirect("/cart")
 
 @app.route("/remove/<key>")
 def remove(key):
-    orderData.pop(key)
+    session["orderData"].pop(key)
+    session.modified = True
     return redirect("/cart")
 
 
